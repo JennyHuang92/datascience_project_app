67be885862462b5e292ce737a79ed3178b225efb
Jenny Huang <siminghuang92@gmail.com>
2024-04-03 10:50:06 -0600

    update on 04032024

--- penguinapp.py
+++ penguinapp.py
@@ -1,3 +1,21 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import numpy as np
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+
+st.write("""
+         
+# Penguin Prediction App
+This app predicts the **Palmer Penguin** species!
+         """)
+
+st.sidebar.header('User Input Features')
+st.sidebar.markdown(
+    """
+
+"""
+)
+
+
